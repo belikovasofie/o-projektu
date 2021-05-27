6743e29b9899311259dd7cf376f21b92e38ce427
Eva Lastuvkova <eva.lastuvkova@gmail.com>
2021-05-27 16:39:52 +0200

    img path

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 import About from './components/About';
 
 import SofiPhoto from '/src/img/sofi.photo.jpg';
-import EvaPhoto from './src/img/evaphoto';
+import EvaPhoto from './src/img/evaphoto.jpg';
 
 // import EvaPhoto from '../src/img/evaphoto.jpg';
 // ./src/img/evaphoto.jpg';
